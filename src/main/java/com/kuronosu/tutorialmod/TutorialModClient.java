15dbcdfcb178a817ce005f2e219b3dbe8e2166f9
Kuronosu22 <guitandrade642@gmail.com>
2025-05-25 00:48:05 -0300

    Initial Commit

--- src/main/java/com/kuronosu/tutorialmod/TutorialModClient.java
+++ src/main/java/com/kuronosu/tutorialmod/TutorialModClient.java
@@ -1,11 +1,12 @@
 package com.kuronosu.tutorialmod;
 
+import com.kuronosu.tutorialmod.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TutorialModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/com/kuronosu/tutorialmod/item/ModItemsGroup.java
@@ -0,0 +1,9 @@
+package com.kuronosu.tutorialmod.item;
+
+import com.kuronosu.tutorialmod.TutorialMod;
+
+public class ModItemsGroup {
+    public static void registerModItemsGroup() {
+        TutorialMod.LOGGER.info("Registering Mod items Group for " + TutorialMod.MOD_ID);
+    }
+}
